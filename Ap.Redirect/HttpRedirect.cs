5d0e4089f60fdfb28455dcc922855a4e8e42750d
alexanderpo <alexander.pozhidaev@gmail.com>
2016-02-08 10:17:34 +0300

    refactoring

--- Ap.Redirect/HttpRedirect.cs
+++ Ap.Redirect/HttpRedirect.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Ap.Redirect
+namespace Redirect
 {
     public class HttpRedirect
     {
--- Ap.Redirect/Properties/AssemblyInfo.cs
+++ Ap.Redirect/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Ap.Redirect")]
+[assembly: AssemblyTitle("Redirect")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ap.Redirect")]
+[assembly: AssemblyProduct("Redirect")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
