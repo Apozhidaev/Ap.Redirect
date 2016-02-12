97e0e3acf6732252b8842beaf0781fc4a162ad20
alexanderpo <alexander.pozhidaev@gmail.com>
2016-02-12 05:28:12 +0300

    refactoring

---
+++ Redirect/ProcessRequestEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Redirect
+{
+    public class ProcessRequestEventArgs : EventArgs
+    {
+        public ProcessRequestEventArgs(Exception exception)
+        {
+            Exception = exception;
+        }
+
+        public Exception Exception { get; }
+    }
+}
---
+++ Redirect/RedirectSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Redirect
+{
+    public class RedirectSettings
+    {
+        public string[] Froms { get; set; }
+        public string To { get; set; }
+        public Dictionary<Regex, string> QueryRules { get; set; }
+        public Dictionary<string, Dictionary<Regex, string>> ContentRules { get; set; }
+    }
+}
